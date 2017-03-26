b2b5132693e90e892a82a474be5edd3c1b06f6c5
AlexGram <agramatik@list.ru>
2017-03-26 21:34:56 +0300

    finished with repeats

--- src/main/java/com/string/Application.java
+++ src/main/java/com/string/Application.java
@@ -5,7 +5,7 @@ package com.string;
  */
 public class Application {
     public static void main(String[] args) {
-        StringBuilder string = new StringBuilder("hheelloohhhh");
+        StringBuilder string = new StringBuilder("HHhherthhellllll");
         int reppets = 1;
 
         String newString = new String();
@@ -13,22 +13,25 @@ public class Application {
             for (int j = (i + 1); j < string.length(); j++) {
                 char ch1 = string.charAt(i);
                 char ch2 = string.charAt(j);
+
                 if (ch1 == ch2) {
+
                     newString = String.valueOf(ch1);
                     reppets += 1;
-                }
+                } else
+                    break;
+
+
             }
             if (reppets > 1) {
                 newString += reppets;
                 string.replace(i, (i + reppets), newString);
                 // string=new StringBuilder(newString);
                 reppets = 1;
-            /*} else
-                System.out.println("");
-*/
+
 
             } else
-                System.out.println();
+                reppets = 1;
         }
         System.out.println(string);
     }
