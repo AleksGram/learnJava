a5c154f4e7c6e574dae51dc49295a16396d7366b
AlexGram <agramatik@list.ru>
2017-03-29 22:48:52 +0300

    finished add method

--- src/main/java/com/absractClass/MyLinkList.java
+++ src/main/java/com/absractClass/MyLinkList.java
@@ -15,7 +15,40 @@ public class MyLinkList implements NodeList {
         return this.root;
     }
 
-    public boolean addItem(ListItem item) {
+    public boolean addItem(ListItem newItem) {
+        if (this.root == null) {
+            this.root = newItem;
+            return true;
+        }
+        ListItem currentItem = this.root;
+        while (root != null) {
+            int comparison = (currentItem.compareTo(newItem));
+            if (comparison < 0) {
+                if (currentItem.next() != null) {
+                    currentItem = currentItem.next();
+                } else {
+                    currentItem.setNext(newItem);
+                    newItem.setPrevious(currentItem);
+                    return true;
+                }
+            } else if (comparison > 0) {
+                if (currentItem.previous() != null) {
+                    currentItem.previous().setNext(newItem);
+                    newItem.setPrevious(currentItem.previous());
+                    newItem.setNext(currentItem);
+                    currentItem.setPrevious(newItem);
+                } else {
+                    newItem.setNext(this.root);
+                    this.root.setPrevious(newItem);
+                    this.root = newItem;
+                }
+                return true;
+            } else {
+                System.out.println(newItem.getValue() + " is already present, adding aborted");
+
+            }
+
+        }
         return false;
     }
 
