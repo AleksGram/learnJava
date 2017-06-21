06300b25f1a155c920b9435e1146d5872f0caf31
Aleksandr Gramatik <agra@kyiv.ciklum.net>
2017-06-21 12:15:52 +0300

    threads & deadlocks

---
+++ src/main/java/com/threads/MyRunnable.java
@@ -0,0 +1,12 @@
+package com.threads;
+
+/**
+ * Created by agra on 6/20/2017.
+ */
+public class MyRunnable implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println("Hello from MyRunnable");
+    }
+}
---
+++ src/main/java/com/treads2/Operations.java
@@ -0,0 +1,65 @@
+package com.treads2;
+
+import javax.naming.InsufficientResourcesException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by agra on 6/21/2017.
+ */
+public class Operations {
+
+    private static final long WAIT_SEC = 2;
+
+    public static void main(String[] args) throws InsufficientResourcesException, InterruptedException {
+        final Account a = new Account(1000, new ReentrantLock() );
+        final Account b = new Account(2000, new ReentrantLock());
+
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    transfer(a, b, 500);
+                    System.out.println("Transfer a=>b successfully completed ");
+                } catch (InsufficientResourcesException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+        transfer(b, a, 300);
+        System.out.println("Transfer b=>a successfully completed ");
+    }
+
+    static void transfer(Account acc1, Account acc2, int amount) throws InsufficientResourcesException, InterruptedException {
+        if (acc1.getLock().tryLock(WAIT_SEC, TimeUnit.SECONDS)) {
+            try {
+                if (acc2.getLock().tryLock(WAIT_SEC, TimeUnit.SECONDS)) {
+
+                    try {
+
+                        if (acc1.getBalance() < amount)
+                            throw new InsufficientResourcesException();
+                      //synchronized (acc1) {
+                            System.out.println("Grabed the lock acc1");
+                          // Thread.sleep(1000); // invoke the deadlock
+                           // synchronized (acc2) {
+                                System.out.println("Grabed the lock acc2");
+
+                                acc1.withdraw(amount);
+                                acc2.deposit(amount);
+                          //  }
+                       // }
+                    } finally {
+                        acc1.getLock().unlock();
+                    }
+                } else {
+                    throw new InsufficientResourcesException();
+                }
+            }catch (InsufficientResourcesException e){
+                e.getMessage();
+            }
+        }
+    }
+}
